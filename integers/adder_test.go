948fb34f4b82f51c411d17b30a185cd22ff2aa63
Jason Hong <jasonhong6@proton.me>
2026-06-16 21:06:07 +0700

    Initial commit

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,6 +12,12 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
 
 // func assertCorrectMessage(t testing.TB, got, want string) {
