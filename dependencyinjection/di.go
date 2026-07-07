be54a6e304d5e7a38eb47d5167ba3c7b9e2254d5
Jason Hong <jasonhong6@proton.me>
2026-07-07 18:30:27 -0500

    Finish di.go

--- dependencyinjection/di.go
+++ dependencyinjection/di.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
-	fmt.Fprintf(writer, "Hello %s", name)
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	// Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
